dbc2885bafde9d193ccb6f0953ef2cdaa25830a1
yangtingting05097224 <3048903923@qq.com>
2021-06-03 17:27:10 +0800

    regsiter phpwind api testcase commit

--- debugtalk.py
+++ debugtalk.py
@@ -1,5 +1,24 @@
 
 import time
+import requests
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def setup_case(name):
+    print('测试用例[%s]开始执行~'%name)
+
+def teardown_case(name):
+    print('测试用例[%s]执行结束~'%name)
+
+def setup_step(name):
+    print('测试步骤[%s]开始执行~'%name)
+
+def teardown_step(name):
+    print('测试步骤[%s]执行结束'%name)
+
+# 生成随机数函数
+
+
+def get_username():
+    pass
