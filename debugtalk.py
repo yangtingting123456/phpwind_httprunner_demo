1b7a6613a3ee6d934922e85a7d57212031b6547b
yangtingting05097224 <3048903923@qq.com>
2021-06-04 17:35:35 +0800

    参数化：variables,testsuits,degtalk.py

--- debugtalk.py
+++ debugtalk.py
@@ -1,6 +1,7 @@
 
 import time
 import requests
+import random
 
 def sleep(n_secs):
     time.sleep(n_secs)
@@ -18,7 +19,9 @@ def teardown_step(name):
     print('测试步骤[%s]执行结束'%name)
 
 # 生成随机数函数
+def get_Randrom():
+    return ''.join(random.sample(['z','y','x','w','v','u','t','s','r','q','p','o','n','m',
+                                  'l','k','j','i','h','g','f','e','d','c','b','a'], 9))
 
 
-def get_username():
-    pass
+# 重复执行测试用例
